c0b654810e7841f68545f37a222e92008539883e
nsmith31415 <nsmith31415@gmail.com>
2020-04-12 22:44:51 -0500

    Changed ctrl to get closer to working I think its good?

--- ctrl.v
+++ ctrl.v
@@ -3,15 +3,15 @@
 // Nick Smith and Alan Rolla
 `timescale 1ns/100ps
 
-module ctrl (clk, rst_f, opcode, mm, stat, rd_sel, rf_we, alu_op, wb_sel, PC_RST, PC_Sel, PC_Write, BR_Sel);
+module ctrl (clk, rst_f, opcode, mm, stat, rd_sel, rf_we, alu_op, wb_sel, PC_RST, PC_Sel, PC_Write, ir_load, BR_Sel, rb_sel);
 
   /* Declare the ports listed above as inputs or outputs.  Note that
      you will add signals for parts 2, 3, and 4. */
   
   input clk, rst_f;
   input [3:0] opcode, mm, stat;
-  output reg rf_we, wb_sel, PC_RST, PC_Sel, PC_Write, BR_Sel;
-  output reg [1:0] alu_op, rd_sel;
+  output reg rf_we, wb_sel, PC_RST, PC_Sel, PC_Write, ir_load, BR_Sel, rb_sel, rd_sel;
+  output reg [1:0] alu_op;
   
   // state parameter declarations
   
@@ -38,12 +38,16 @@ module ctrl (clk, rst_f, opcode, mm, stat, rd_sel, rf_we, alu_op, wb_sel, PC_RST
      edge of the clock, OR resets the state to 'start1' on the negative edge
      of rst_f. Notice that the computer is reset when rst_f is low, not high. */
 
-  always @(posedge clk, negedge rst_f)
+  always @(posedge clk)
+	begin
+	PC_RST=1'b0;
+	present_state<=next_state;
+	end
+  always @(negedge clk)
   begin
     if (rst_f == 1'b0)
-      present_state <= start1;
-    else
-      present_state <= next_state;
+	PC_RST=1'b1;
+      	present_state <= start1;
   end
   
   /* Combinational procedure that determines the next state of the fsm. */
@@ -51,15 +55,13 @@ module ctrl (clk, rst_f, opcode, mm, stat, rd_sel, rf_we, alu_op, wb_sel, PC_RST
   always @(present_state, rst_f)
   begin
     case(present_state)
-      start0:	 next_state <= start1;
-      start1:	 next_state <= decode;
-      fetch:	 next_state <= decode;
-      decode:	 next_state <= execute;
-      execute:	 next_state <= mem;
-      mem:	 next_state <= writeback;
-      writeback: next_state <= fetch;
-      default:
-        next_state = start1;
+      start0:	 begin next_state <= start1; end
+      start1:	 begin next_state <= fetch; end
+      fetch:	 begin next_state <= decode; end
+      decode:	 begin next_state <= execute; end
+      execute:	 begin next_state <= mem; end
+      mem:	 begin next_state <= writeback; end
+      writeback: begin next_state <= fetch; end
     endcase
   end
   
@@ -67,69 +69,94 @@ module ctrl (clk, rst_f, opcode, mm, stat, rd_sel, rf_we, alu_op, wb_sel, PC_RST
 //Check on rising clock edges
 always @(posedge clk)
 	begin
-	//first check for no operation
-	if(opcode == NOOP)
+	//first check state
+	if(present_state == start0)
 		begin
-		rf_we<=0;
-		alu_op<=2'b00;
-		wb_sel<=0;
-		PC_Sel<=0;
-		PC_RST<=0;
-		BR_Sel<=0;
+		rf_we <=1'b0;
+		wb_sel<=1'b0;
+		alu_op<=2'b10;
+		rb_sel <= 1'b0;
+		PC_Write <= 1'b0;	
+		ir_load <= 1'b0;
+		BR_Sel <= 1'b0;
+		PC_RST <= 1'b1;
+		PC_Sel <= 1'b0;
 		end
-	//second check state
+	if(present_state == start1)
+		begin
+		alu_op <= 2'b00;
+		ir_load <= 1'b0;
+		PC_RST <= 1'b0;
+		end
 	if(present_state == fetch)
 		begin
-		PC_Write<=1;
-		PC_RST<=0;
-		PC_Sel<=0;
-			
-		rf_we<=0;
+		$display("Fetching");
+		rf_we <= 1'b0;
+		wb_sel <= 1'b0;
 		alu_op <= 2'b00;
-		wb_sel<=0;
-		//third check opcode
-		if(opcode == ALU_OP) 
-			begin
-			rf_we=1'b0;
-			alu_op=2'b00;
-			wb_sel=1'b0;
-			BR_Sel<=0;
-			PC_Sel<=0;
-			
-			end
-		if((	   opcode==BNE)	
-			||(opcode==BRA)	
-			||(opcode==BRR))	
-			begin
-			wb_sel<=0;
-			rf_we<=0;
-			alu_op<=2'b10;
-			end
+		rb_sel <= 1'b0;
+		PC_Write <= 1'b1;	// Must inc pc in fetch
+		ir_load <= 1'b1;
+		BR_Sel <= 1'b0;
+		PC_RST <= 1'b0;
+		PC_Sel <= 1'b0;	
 		end
-		
-	else if (present_state == decode)
+	if(present_state == decode)
 		begin
-		PC_Write<=0;
-		if(opcode == 4'b1000) //opcode for ALU op
+		PC_Write<=1'b0;
+		ir_load<=1'b0;
+		$display("Decoding");
+		//check opcode (decoding only matters for BNE,BRA, BRR, BNR right now)
+	
+		if(opcode==BNE)
 			begin
-				if(mm==4'b1000)
-					begin
-					rd_sel<=1;
-					end
-				else
-					begin
-					rd_sel<=0;
-					end
+			PC_Sel<=1;
+			if((stat & mm)==4'b0000)	
+				begin
+				$display("Branching BNE");
+				PC_Sel<=1;
+				PC_Write<=1;
+				BR_Sel<=1;
+				end
 			end
-		else
+		if(opcode==BNR)
 			begin
-			rd_sel<=0;
+			if((stat & mm)==4'b0000)	
+				begin
+				$display("Branching BNR");
+				PC_Sel<=1;
+				PC_Write<=1;
+				BR_Sel<=0;
+				end
+			end
+		if(opcode==BRA)
+			begin
+			if((stat & mm)!=4'b0000)	
+				begin
+				$display("Branching BRA");
+				PC_Sel<=1;
+				PC_Write<=1;
+				BR_Sel<=1;
+				end
+			end
+		if(opcode==BRR)
+			begin
+			if((stat & mm)!=4'b0000)	
+				begin
+				$display("Branching BRR");
+				PC_Sel<=1;
+				PC_Write<=1;
+				BR_Sel<=0;
+				end
 			end
 		end
+	
 	else if (present_state == execute)
 		begin
-		
-		//third check opcode
+		$display("Executing");
+		PC_Write<=1'b0;
+		ir_load<=1'b0;
+		//check MM
 		if(opcode == 4'b1000) //opcode for ALU op
 			begin
 				//fourth check mm, if 1000, use immediate load instead of input B
@@ -142,61 +169,30 @@ always @(posedge clk)
 					alu_op<=2'b00;
 					end
 			end
-		if(opcode==BNE)
+		else		//if not ALU_OP
 			begin
-			if((mm&stat)==4'b0000)
-				begin
-				PC_Sel<=1; 
-				BR_Sel<=1;
-				present_state<=fetch;
-				end
-			else
-				begin
-				PC_Sel<=0;
-				end
+				//fourth check mm
+				if(mm==4'b1000)
+					begin
+					alu_op<=2'b11;
+					end
+				else
+					begin
+					alu_op<=2'b10;
+					end
 			end
 		
-		
-		if(opcode==BRR)
-			begin
-			if((mm&stat)!=4'b0000)
-				begin
-				PC_Sel<=1; 
-				BR_Sel<=0;
-				present_state<=fetch;
-				end
-			else
-				begin
-				PC_Sel<=0;
-				end
-			end
-		
-
-		if(opcode==BRA)
-			begin
-			if((mm&stat)!=4'b0000)
-				begin
-				PC_Sel<=1; 
-				BR_Sel<=1;
-				present_state<=fetch;
-				end
-			else
-				begin
-				PC_Sel<=0;
-				end
-			end
 		end
 	else if (present_state == mem)
 		begin
-		//third check opcode
-		if(opcode == 4'b1000) //opcode for ALU op
-			begin
-			rf_we=1;
-			end
+		
 		end
 	else if (present_state == writeback)
 		begin
-			//writeback state not yet used
+		if(opcode==4'b1000)
+			begin
+			rf_we=1;
+			end
 		end
 	end
 	
